b36e5d105427ee96b231f5ca7ae008e6fb9f557c
phpngn <92076342+phpngn@users.noreply.github.com>
2022-10-12 15:48:50 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,19 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+	
+	@Test
+  public void testAnotherReverseInPlace() {
+    int[] input1 = { 0, 1, 2 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 2, 1, 0 }, input1);
+    
+
+	}
+
+  @Test
+  public void testReversedLength5() {
+    int[] input1 = {1, 2, 3, 4, 5}; 
+    assertArrayEquals(new int[] {5, 4, 3, 2, 1}, ArrayExamples.reversed(input1));
+  }
 }
